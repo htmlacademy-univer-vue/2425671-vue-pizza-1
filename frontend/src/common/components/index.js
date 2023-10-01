0750296e202c89517ba234a740c11c08597fbb6f
Artem Bondar <artemiy.bondar49@gmail.com>
2023-10-01 18:12:12 +0300

    добавил общие компоненты

--- frontend/src/common/components/index.js
+++ frontend/src/common/components/index.js
@@ -3,4 +3,5 @@ export { default as CounterInput } from "./CounterInput.vue";
 export { default as NormalButton } from "./NormalButton.vue";
 export { default as Title } from "./Title.vue";
 export { default as Sheet } from "./Sheet.vue";
-// export { default as RadioButton } from "./RadioButton.vue";
+export { default as AppDrag } from "./AppDrag.vue";
+export { default as AppDrop } from "./AppDrop.vue";
