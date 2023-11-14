146cf517d317d8415ebea5f995cfca5deaef18bc
Artem Bondar <artemiy.bondar49@gmail.com>
2023-11-14 17:16:54 +0300

    добавил ещё геттеров

--- frontend/src/stores/profile.js
+++ frontend/src/stores/profile.js
@@ -3,15 +3,35 @@ import { defineStore } from 'pinia'
 export const useProfileStore = defineStore('profile', {
 	state: () => ({
 		profile: {
-			id: "",
+			id: 0,
 			name: "",
 			email: "",
 			avatar: "",
 			phone: "",
-			addresses:[]
+			addresses:[],
+			orders: []
 		},
 	}),
-	getters: {},
+	getters: {
+		getAddresses: (state)=> {
+			return state.addresses;
+		},
+		getOrders: (state)=> {
+			return state.orders;
+		},
+		getName: (state)=> {
+			return state.name;
+		},
+		getEmail: (state)=> {
+			return state.email;
+		},
+		getAvatar: (state)=> {
+			return state.avatar;
+		},
+		getPhone: (state)=> {
+			return state.phone;
+		}
+	},
 	actions: {}
 })
 
